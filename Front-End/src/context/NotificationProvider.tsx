997c2e6cac896fc19161dcdde54a0b7cc70b5100
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-01-23 17:40:09 +0400

    after editing the repitions of the notification attributes

--- Front-End/src/context/NotificationProvider.tsx
+++ Front-End/src/context/NotificationProvider.tsx
@@ -1,24 +1,22 @@
 import React from "react";
-import { ToastContainer, toast, Slide } from "react-toastify";
+import { ToastContainer, toast, Slide, ToastOptions, ToastPosition } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 export const notifySuccess = () => {
-  toast.success("Success", {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "light",
-    transition: Slide,
-  });
+  toast.success("Success", commonAttributes);
 };
 
 export const notifyWarning = () => {
-  toast.warn("Warning", {
-    position: "top-right",
+  toast.warn("Warning", commonAttributes);
+};
+
+export const notifyError = () => {
+  toast.error('Error occurred', commonAttributes);
+};
+
+const commonAttributes: ToastOptions<unknown> = {
+    position: "top-right" as ToastPosition,
     autoClose: 5000,
     hideProgressBar: false,
     closeOnClick: true,
@@ -27,23 +25,7 @@ export const notifyWarning = () => {
     progress: undefined,
     theme: "light",
     transition: Slide,
-  });
-};
-
-export const notifyError = () => {
-    toast.error('Error occured', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        transition: Slide,
-        });
-        
-}
+  };
 
 function NotificationProvider() {
   return (
