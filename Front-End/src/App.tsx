71bbadebe926afa45c89ee3acc8c177267321325
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-01-23 08:02:25 +0400

    added the login and sign up page

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -3,13 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home.tsx'
-  
+
 function App() {
 
   return (
     <>
-     
-
       <Home></Home>
     </>
   )
