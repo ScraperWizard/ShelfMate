9bff979c5874cde9e0985dee738a6e75e758cbf6
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-01-24 20:52:44 +0400

    added the base of authentacation

--- Front-End/src/pages/Login.tsx
+++ Front-End/src/pages/Login.tsx
@@ -1,44 +1,83 @@
-import { useState } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import AnimatedPage from "../Animation/AnimatedPage";
-import { showNotification, Notification } from "../context/NotificationProvider";
+import {
+  showNotification,
+  NotificationProvider,
+  Notification,
+} from "../context/NotificationProvider";
 import socket from "../Socket";
 
 function Login() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (HTMLInputElement) => {
-    setUsername
-  }
-  // Test
-  // showNotification({
-  //   type: "success",
-  //   message: "This is a success notification",
-  // } as Notification);
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
 
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-  function submit() {
-    socket.emit("authenticate", {
-      // userrname
-      // password
-    })
-  }
-  
+  const submit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    socket.emit("authenticate", { username, password });
+  };
+
+  socket.on("authenticated", ({ success, message }) => {
+    if (success) {
+      showNotification({
+        type: "success",
+        message: "Authentication successful",
+      } as Notification);
+    } else {
+      showNotification({
+        type: "error",
+        message: `Authentication failed: ${message}`,
+      } as Notification);
+    }
+  });
+
   return (
     <AnimatedPage>
-      <div className="relative w-full h-screen bg-gradient-to-r from-white via-blue-900 to-slate-900" data-name="login">
+      <div
+        className="relative w-full h-screen bg-gradient-to-r from-white via-blue-900 to-slate-900"
+        data-name="login"
+      >
         <div className="flex justify-center items-center h-full">
-          <form className="max-w-[400px] w-full mx-auto bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
-            <h2 className="text-4xl font-bold text-center py-4 text-white hover:text-slate-600">Login</h2>
+          <form
+            className="max-w-[400px] w-full mx-auto bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative"
+            onSubmit={submit}
+          >
+            <h2 className="text-4xl font-bold text-center py-4 text-white hover:text-slate-600">
+              Login
+            </h2>
 
             <div className="flex flex-col mb-4">
-              <label className="text-slate-400 hover:text-sky-400">Enter your username: </label>
-              <input className="border relative bg-gray-100 p-2" onChange={setUsername} type="text" placeholder="username" />
+              <label className="text-slate-400 hover:text-sky-400">
+                Enter your username:{" "}
+              </label>
+              <input
+                className="border relative bg-gray-100 p-2"
+                onChange={handleUsernameChange}
+                type="text"
+                placeholder="username"
+              />
             </div>
             <div className="flex flex-col ">
-              <label className="text-slate-400 hover:text-sky-400">Enter your password: </label>
-              <input className="border relative bg-gray-100 p-2" type="password" placeholder="password" />
+              <label className="text-slate-400 hover:text-sky-400">
+                Enter your password:{" "}
+              </label>
+              <input
+                className="border relative bg-gray-100 p-2"
+                onChange={handlePasswordChange}
+                type="password"
+                placeholder="password"
+              />
             </div>
-            <button className="w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative text-white hover:scale-110 duration-200">Sign In</button>
+            <button className="w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative text-white hover:scale-110 duration-200">
+              Sign In
+            </button>
             <p className='flex items-center mt-2 text-slate-400 hover:text-sky-400"'>
               <input className="mr-2 " type="checkbox" />
               Remember Me
@@ -52,4 +91,5 @@ function Login() {
     </AnimatedPage>
   );
 }
+
 export default Login;
