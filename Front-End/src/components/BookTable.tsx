9a1810bc80a3d74461cbd8c60ce0dec19cc20921
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-02-05 19:05:40 +0400

    now everything I set up the requests for returning and borrowing the books according to the user

--- Front-End/src/components/BookTable.tsx
+++ Front-End/src/components/BookTable.tsx
@@ -7,32 +7,72 @@ import book4 from "../assets/book_4.jpg";
 import book5 from "../assets/book_5.jpg";
 import book6 from "../assets/book_7.png";
 import book7 from "../assets/book_8.png";
-import '../styles/myBooks.css'
+import "../styles/myBooks.css";
 import socket from "../Socket";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+import { useAuth } from "../context/AuthProvider";
 
+type Book = {
+  id: number;
+  image: string;
+  genre: string;
+  title: string;
+};
 function BookTable() {
-  const [returnedBooks, setReturnedBooks] = useState([]);
+  const [borrowedBooks, setBorrowedBooks] = useState<Book[]>([]);
+  const { user } = useAuth();
 
   useEffect(() => {
+    if (user) {
+      socket.emit("get-borrowed-books", { username: user.username });
 
-      socket.emit("getReturnedBooks");
-
-      socket.on("returnedBooks", (data) => {
-          setReturnedBooks(data);
+      socket.on("borrowed-books-response", (response: Book[]) => {
+        setBorrowedBooks(response);
       });
 
       return () => {
-
-          socket.off("returnedBooks");
+        socket.off("borrowed-books-response");
       };
-  }, []);
+    }
+  }, [user]);
+
+  const handleReturn = (bookId: number) => {
+    socket.emit("return-book", { bookId, borrower: user?.username });
+
+    setBorrowedBooks((prevBooks) =>
+      prevBooks.filter((book) => book.id !== bookId)
+    );
+  };
   return (
     <>
       <Navbar></Navbar>
       <div className="myBooks" data-name="book-table">
         <h1>My Books</h1>
         <div className="myBooksBox">
+          {borrowedBooks.map((book) => (
+            <div className="myBooksCard" key={book.id}>
+              <div className="myBooksImg">
+                <img src={book.image} className="Img" />
+                <div className="myBoogTag">
+                  <p className="writer">{book.title}</p>
+                  <div className="categories">{book.genre}</div>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleReturn(book.id);
+                    }}
+                    className="returnBtn"
+                  >
+                    Return
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+
+
+        {/* this is like a placeholder done by yaman and I will remove it as soon as I check everything is right */}
           <div className="myBooksCard">
             <div className="myBooksImg">
               <img src={book1} className="Img" />
@@ -49,7 +89,7 @@ function BookTable() {
 
           <div className="myBooksCard">
             <div className="myBooksImg">
-              <img src={book2} className="Img"/>
+              <img src={book2} className="Img" />
             </div>
 
             <div className="myBoogTag">
@@ -63,7 +103,7 @@ function BookTable() {
 
           <div className="myBooksCard">
             <div className="myBooksImg">
-              <img src={book3} className="Img"/>
+              <img src={book3} className="Img" />
             </div>
 
             <div className="myBoogTag">
@@ -77,7 +117,7 @@ function BookTable() {
 
           <div className="myBooksCard">
             <div className="myBooksImg">
-              <img src={book4} className="Img"/>
+              <img src={book4} className="Img" />
             </div>
 
             <div className="myBoogTag">
@@ -91,7 +131,7 @@ function BookTable() {
 
           <div className="myBooksCard">
             <div className="myBooksImg">
-              <img src={book5} className="Img"/>
+              <img src={book5} className="Img" />
             </div>
 
             <div className="myBoogTag">
@@ -105,7 +145,7 @@ function BookTable() {
 
           <div className="myBooksCard">
             <div className="myBooksImg">
-              <img src={book6} className="Img"/>
+              <img src={book6} className="Img" />
             </div>
 
             <div className="myBoogTag">
@@ -119,7 +159,7 @@ function BookTable() {
 
           <div className="myBooksCard">
             <div className="myBooksImg">
-              <img src={book7} className="Img"/>
+              <img src={book7} className="Img" />
             </div>
 
             <div className="myBoogTag">
--- Front-End/src/pages/Library.tsx
+++ Front-End/src/pages/Library.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
 import arrivalImg1 from "../assets/arrival_1.jpg";
 import arrivalImg2 from "../assets/arrival_2.jpg";
 import arrivalImg3 from "../assets/arrival_3.jpg";
@@ -17,32 +17,76 @@ import bookImg12 from "../assets/book_12.png";
 import bookImg13 from "../assets/book_13.png";
 import bookImg14 from "../assets/book_14.png";
 import bookImg15 from "../assets/book_15.png";
-import '../styles/Library.css'
+import "../styles/Library.css";
 import socket from "../Socket";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
+import {useAuth} from '../context/AuthProvider'
+
+type Book = {
+  id: number;
+  image: string;
+  genre: string;
+  title: string;
+};
 
 function Library() {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState<Book[]>([]);
+  const { user } = useAuth();
+  useEffect(() => {
+    if (user) {
+     
+      socket.emit("get-library-books", { username: user.username });
 
-    useEffect(() => {
-       // to get the books that can be borrowed
-        socket.emit("getAvailableBooks");
+      socket.on("library-books-response", (response: Book[]) => {
+        setBooks(response);
+      });
 
-        socket.on("availableBooks", (data) => {
-            setBooks(data);
-        });
+      return () => {
+        socket.off("library-books-response");
+      };
+    }
+  }, [user]);
 
-        
-    }, []);
+  const handleBorrow = (bookId: number) => {
+    socket.emit("borrow-book", { bookId, borrower: user?.username });
 
+    setBooks((prevBooks) => prevBooks.filter((book) => book.id !== bookId));
+  };
   return (
     <div className="library" data-name="library">
-        <Navbar></Navbar>
+      <Navbar></Navbar>
       <h1>Library</h1>
       <div className="libraryBox">
+        {/* this is where I will loop through each book  */}
+        {books.map((book) => (
+          <div className="libraryCards" key={book.id}>
+            <div className="libraryImage">
+              <img src={book.image} className="Image" alt={book.title} />
+            </div>
+            <div className="libraryTag">
+              <p>{book.genre}</p>
+              <div className="libraryIcons">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star-half-stroke"></i>
+              </div>
+              <a href="#" className="borrowBtn" onClick={(e) => {
+                  e.preventDefault();
+                  handleBorrow(book.id);
+                }}>
+                Borrow
+              </a>
+            </div>
+          </div>
+        ))}
+
+        {/* this is like a placeholder done by yaman I will remove it as soon as I make sure that everything works right */}
+
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg1} className="Image"/>
+            <img src={arrivalImg1} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Romance, Mystery</p>
@@ -61,7 +105,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg2} className="Image"/>
+            <img src={arrivalImg2} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Action</p>
@@ -80,7 +124,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg3} className="Image"/>
+            <img src={arrivalImg3} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Police Novel</p>
@@ -99,7 +143,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg4} className="Image"/>
+            <img src={arrivalImg4} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Adventure</p>
@@ -118,7 +162,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg5} className="Image"/>
+            <img src={arrivalImg5} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Comedy</p>
@@ -137,7 +181,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg6} className="Image"/>
+            <img src={arrivalImg6} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Magic, Adult</p>
@@ -156,7 +200,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg7} className="Image"/>
+            <img src={arrivalImg7} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Science and Nature</p>
@@ -175,7 +219,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg8} className="Image"/>
+            <img src={arrivalImg8} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Cartoon</p>
@@ -194,7 +238,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg9} className="Image"/>
+            <img src={arrivalImg9} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Philosophy</p>
@@ -213,7 +257,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={arrivalImg10} className="Image"/>
+            <img src={arrivalImg10} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Science Fiction</p>
@@ -232,7 +276,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={bookImg9} className="Image"/>
+            <img src={bookImg9} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Action, Mystery</p>
@@ -251,7 +295,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={bookImg10} className="Image"/>
+            <img src={bookImg10} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Business & Finance</p>
@@ -270,7 +314,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={bookImg11} className="Image"/>
+            <img src={bookImg11} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Drama</p>
@@ -289,7 +333,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={bookImg12} className="Image"/>
+            <img src={bookImg12} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Business and Wellness</p>
@@ -308,7 +352,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={bookImg13} className="Image"/>
+            <img src={bookImg13} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Mindfulness</p>
@@ -327,7 +371,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={bookImg14} className="Image"/>
+            <img src={bookImg14} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Kids</p>
@@ -346,7 +390,7 @@ function Library() {
 
         <div className="libraryCards">
           <div className="libraryImage">
-            <img src={bookImg15} className="Image"/>
+            <img src={bookImg15} className="Image" />
           </div>
           <div className="libraryTag">
             <p>Law</p>
