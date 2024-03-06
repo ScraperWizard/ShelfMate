2f2ea456b7080455373c3716052a362039a06c9d
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-03-06 23:23:13 +0400

    minor change (not fixed)

--- Front-End/src/pages/Users-enrollment/EnrollUser.tsx
+++ Front-End/src/pages/Users-enrollment/EnrollUser.tsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 import socket from "../../Socket";
 
 interface Request {
-    image: string;
-    title: string;
-    userId: string;
-    barcode: string;
-    id: string;
-    date: string;
-  }
+  image: string;
+  title: string;
+  userID: string;
+  barcode: string;
+  id: number;
+  date: string;
+}
 function BookRequests() {
-    const [requests, setRequests] = useState<Request[]>([]);
+  const [requests, setRequests] = useState<Request[]>([]);
 
   useEffect(() => {
     socket.emit("get-requests", {});
@@ -24,14 +24,29 @@ function BookRequests() {
     };
   }, []);
 
-  console.log("This is the value of request after reseting", requests);
+  const handleRequest = (bookId: number, accteptance: boolean) => {
+    socket.emit("handle-request", { bookId: bookId, acceptance: accteptance });
+    console.log("handle request emitted");
+    socket.on("accept-request-response", (response) => {
+      console.log(
+        "This is the response from accept-request-response",
+        response
+      );
+    });
+
+    return () => {
+      socket.off("get-requests-response");
+    };
+  };
 
   return (
     <div data-name="get-requests" className="container mx-auto">
-      
-        {requests.map((request, index) => (
-            <div className="max-w-xl mx-auto mb-5">
-          <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden mb-4">
+      {requests.map((request, index) => (
+        <div className="max-w-xl mx-auto mb-5">
+          <div
+            key={index}
+            className="bg-white rounded-lg shadow-md overflow-hidden mb-4"
+          >
             <div className="flex items-center p-4">
               <div className="w-24 h-24 mr-4">
                 <img
@@ -41,15 +56,21 @@ function BookRequests() {
                 />
               </div>
               <div>
-                <h2 className="text-lg font-bold text-gray-800">{request.title}</h2>
+                <h2 className="text-lg font-bold text-gray-800">
+                  {request.title}
+                </h2>
                 <div className="grid grid-cols-2 gap-2 mt-2">
                   <div>
                     <p className="text-gray-600">User ID:</p>
-                    <p className="text-gray-800 font-semibold">{request.userId}</p>
+                    <p className="text-gray-800 font-semibold">
+                      {request.userID}
+                    </p>
                   </div>
                   <div>
                     <p className="text-gray-600">Barcode:</p>
-                    <p className="text-gray-800 font-semibold">{request.barcode}</p>
+                    <p className="text-gray-800 font-semibold">
+                      {request.barcode}
+                    </p>
                   </div>
                   <div>
                     <p className="text-gray-600">ID:</p>
@@ -57,23 +78,30 @@ function BookRequests() {
                   </div>
                   <div>
                     <p className="text-gray-600">Date:</p>
-                    <p className="text-gray-800 font-semibold">{request.date}</p>
+                    <p className="text-gray-800 font-semibold">
+                      {request.date}
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
             <div className="bg-gray-100 px-4 py-2">
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 mr-4">
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 mr-4"
+                onClick={() => handleRequest(request.id, true)}
+              >
                 accept
               </button>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300">
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300"
+                onClick={() => handleRequest(request.id, false)}
+              >
                 reject
               </button>
             </div>
           </div>
-          </div>
-        ))}
-     
+        </div>
+      ))}
     </div>
   );
 }
