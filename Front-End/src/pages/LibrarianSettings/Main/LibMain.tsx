604530829d7af03ee9a38efab5533f6223cbcfaa
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-03-06 09:02:46 +0400

    sorry but this is another minor change

--- Front-End/src/pages/LibrarianSettings/Main/LibMain.tsx
+++ Front-End/src/pages/LibrarianSettings/Main/LibMain.tsx
@@ -7,7 +7,7 @@ function LibMain() {
     <div className="flex justify-center mt-[200px] h-screen" data-name="LibMain">
       <div className="text-center">
         <h1 className="text-3xl font-bold mb-4">Welcome, {username}!</h1>
-        <p className="text-lg">You are logged in as an administrator.</p>
+        <p className="text-lg">You are logged in as a Librarian.</p>
         <p className="text-lg">What would you like to do?</p>
       </div>
     </div>
