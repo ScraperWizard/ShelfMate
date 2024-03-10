274d7eec81f082cc9e622ebc1e767cc560793466
Abdulrahman Alali <22000577@ug.buid.ac.ae>
2024-03-11 00:05:05 +0400

    Added public hosting

--- Front-End/src/Socket.tsx
+++ Front-End/src/Socket.tsx
@@ -1,7 +1,7 @@
 import io from "socket.io-client"
 
 const isProduction = process.env.NODE_ENV === 'production';
-const serverURL = isProduction ? "PLACE_PRODUCTION_URL" : "http://localhost:4000";
+const serverURL = isProduction ? "http://217.160.159.152:4000" : "http://localhost:4000";
 const socket = io(serverURL);
 
 socket.on("connect", () => {
